aeb28706f82057e80d5bc8dec5e76ba0d7789cd5
Jimmysb <jbailey@jimmysoftllc.com>
2022-09-19 10:59:26 -0400

    fix cat bug, filter exercises

--- src/components/componentLists/todaysWorkouts/TodaysWorkoutCard.tsx
+++ src/components/componentLists/todaysWorkouts/TodaysWorkoutCard.tsx
@@ -19,11 +19,25 @@ const TodaysWorkoutCard: any = ({ Workout }: any) => {
 
   const { exercises } = dataAndMethodsContext;
 
+  const exercisesFromWorkout = () => {
+    const dude = [];
+    for (let i = 0; i < Workout.exerciseIdsJSON.length; i++) {
+      for (let j = 0; j < exercises.length; j++) {
+        if (Workout.exerciseIdsJSON[i] === exercises[j].id) {
+          dude.push(exercises[j]);
+        }
+      }
+    }
+    return dude;
+  };
+
+  const myExercises = exercisesFromWorkout();
+
   return (
     <div className='card'>
       <h4>{Workout.title}</h4>
       <div className={classes.root}>
-        <TodaysExercises exercises={exercises} />
+        <TodaysExercises exercises={myExercises} />
       </div>
     </div>
   );
--- src/model/exercise/getExercises.tsx
+++ src/model/exercise/getExercises.tsx
@@ -1,55 +1,68 @@
 import batchGetItemDynamoDB from '../../api/batchGetItemDynamoDB';
 
 import {
-    exercisesTableName,
-    projectionExpressionExercise,
-    blankPlaceHolder,
+  exercisesTableName,
+  projectionExpressionExercise,
+  blankPlaceHolder,
 } from '../../api/apiConstants';
 
 const getBatch = async (ids: any) => {
-    let exercises = []
-    const data = await batchGetItemDynamoDB(exercisesTableName, ids, projectionExpressionExercise)
-    if (data.err) {
-        return [];
-    }
-    exercises = data.payload.Responses.exercises;
-    for (let i = 0; i < exercises.length; i++) {
-        exercises[i].title = exercises[i].title === blankPlaceHolder ? '' : exercises[i].title
-        exercises[i].dataJSON = exercises[i].dataJSON === undefined ? JSON.parse('[]') : JSON.parse(exercises[i].dataJSON)
-        exercises[i].categoryJSON = exercises[i].categoryJSON === blankPlaceHolder ? '' : exercises[i].categoryJSON
-    }
-    return exercises;
-}
+  let exercises = [];
+  const data = await batchGetItemDynamoDB(
+    exercisesTableName,
+    ids,
+    projectionExpressionExercise
+  );
+  if (data.err) {
+    return [];
+  }
+  exercises = data.payload.Responses.exercises;
+  for (let i = 0; i < exercises.length; i++) {
+    exercises[i].title =
+      exercises[i].title === blankPlaceHolder ? '' : exercises[i].title;
+    exercises[i].dataJSON =
+      exercises[i].dataJSON === undefined
+        ? JSON.parse('[]')
+        : JSON.parse(exercises[i].dataJSON);
+    exercises[i].categoryJSON =
+      exercises[i].categoryJSON === undefined
+        ? JSON.parse('[]')
+        : JSON.parse(exercises[i].categoryJSON);
+  }
+  return exercises;
+};
 
 const getExercises = async (exerciseItemIds: any) => {
-    if (exerciseItemIds.length === 0) { return [] }
+  if (exerciseItemIds.length === 0) {
+    return [];
+  }
 
-    // get records in batches of 100
-    let myIds = [];
-    let currentIndex = 0;
-    let nextIndex = 0;
-    let myExercises: any = []
-    for (let i = 0; i < exerciseItemIds.length; i++) {
-        myIds.push(exerciseItemIds[i]);
-        currentIndex++;
-        if (currentIndex > 99) {
-            const myBatch = await getBatch(myIds);
-            myIds = [];
-            currentIndex = 0
-            myExercises = myExercises.concat(myBatch)
-            nextIndex = i + 1;
-        }
+  // get records in batches of 100
+  let myIds = [];
+  let currentIndex = 0;
+  let nextIndex = 0;
+  let myExercises: any = [];
+  for (let i = 0; i < exerciseItemIds.length; i++) {
+    myIds.push(exerciseItemIds[i]);
+    currentIndex++;
+    if (currentIndex > 99) {
+      const myBatch = await getBatch(myIds);
+      myIds = [];
+      currentIndex = 0;
+      myExercises = myExercises.concat(myBatch);
+      nextIndex = i + 1;
     }
+  }
 
-    // get any leftover records
-    myIds = [];
-    for (let i = nextIndex; i < exerciseItemIds.length; i++) {
-        myIds.push(exerciseItemIds[i]);
-    }
-    const myBatch = await getBatch(myIds);
-    myExercises = myExercises.concat(myBatch)
+  // get any leftover records
+  myIds = [];
+  for (let i = nextIndex; i < exerciseItemIds.length; i++) {
+    myIds.push(exerciseItemIds[i]);
+  }
+  const myBatch = await getBatch(myIds);
+  myExercises = myExercises.concat(myBatch);
 
-    return myExercises;
-}
+  return myExercises;
+};
 
-export default getExercises
+export default getExercises;
