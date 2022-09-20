630ecd2d2ce93a8d95a73bcadf05840e5accb241
Jimmysb <jbailey@jimmysoftllc.com>
2022-09-20 08:26:32 -0400

    update exercise buttons

--- src/components/componentLists/todaysExercises/todaysExerciseCard.tsx
+++ src/components/componentLists/todaysExercises/todaysExerciseCard.tsx
@@ -13,16 +13,36 @@ const TodaysExercisesCard = ({ Exercise }: any) => {
 
   const dataJSONString = changeToMultiline(Exercise.dataJSON);
 
-  const [count, setCount] = useState(changeToMultiline(Exercise.dataJSON));
+  const [startEdit, setModeStartEdit] = useState(true);
+  const [inDatabase, setInDatabase] = useState(false);
+  const [actual, setActual] = useState('');
+  const [newGoal, setNewGoal] = useState('');
 
   const minRows = Exercise.dataJSON.length;
 
-  const changeData = (e: any) => {
-    setCount(e.target.value);
+  const changeActual = (e: any) => {
+    setActual(e.target.value);
     //const changedData = e.target.value.split(/\r?\n/);
   };
 
-  const handleClickExerciseEdit = (id: any) => {};
+  const changeNewGoal = (e: any) => {
+    setNewGoal(e.target.value);
+    //const changedData = e.target.value.split(/\r?\n/);
+  };
+
+  const handleStartClick = () => {
+    setModeStartEdit(false);
+    if (actual === '') setActual(dataJSONString);
+  };
+
+  const handleCopyClick = () => {
+    setNewGoal(actual);
+  };
+
+  const handleDoneClick = () => {
+    setModeStartEdit(true);
+    setInDatabase(true);
+  };
 
   return (
     <>
@@ -30,25 +50,35 @@ const TodaysExercisesCard = ({ Exercise }: any) => {
         {Exercise.title}
         <span className='mx-1'></span>
         <Button
+          disabled={!startEdit}
           variant='outlined'
           color='primary'
-          onClick={() => handleClickExerciseEdit(Exercise.id)}
+          onClick={() => handleStartClick()}
         >
-          <i className='fas fa-play'></i>
+          {!inDatabase && <i className='fas fa-play'></i>}
+          {inDatabase && <i className='fas fa-edit'></i>}
         </Button>
         <Button
-          disabled={true}
+          disabled={startEdit}
           variant='outlined'
           color='primary'
-          onClick={() => handleClickExerciseEdit(Exercise.id)}
+          onClick={() => handleCopyClick()}
         >
-          <i className='fas fa-stop'></i>
+          <i className='fas fa-copy'></i>
+        </Button>
+        <Button
+          disabled={startEdit}
+          variant='outlined'
+          color='primary'
+          onClick={() => handleDoneClick()}
+        >
+          <i className='fas fa-check'></i>
         </Button>
       </h5>
       <div>
         <TextField
           label='Goal'
-          id='data'
+          id='goal'
           type='text'
           multiline={true}
           minRows={minRows}
@@ -57,12 +87,23 @@ const TodaysExercisesCard = ({ Exercise }: any) => {
         />
         <TextField
           label='Actual'
-          id='data'
+          id='actual'
           type='text'
           multiline={true}
           minRows={minRows}
-          value={count}
-          onChange={changeData}
+          value={actual}
+          onChange={changeActual}
+          disabled={startEdit}
+        />
+        <TextField
+          label='New Goal'
+          id='newGoal'
+          type='text'
+          multiline={true}
+          minRows={minRows}
+          value={newGoal}
+          onChange={changeNewGoal}
+          disabled={startEdit}
         />
       </div>
     </>
