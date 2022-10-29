d08ce812d23b38a9f7f8df0af031ee8094568299
Jimmysb <jbailey@jimmysoftllc.com>
2022-10-28 20:16:32 -0400

    removed comments

--- src/utilities/convertToActualData.tsx
+++ src/utilities/convertToActualData.tsx
@@ -1,9 +1,4 @@
 const convertToActualData = (receivedVal: any): any => {
-  // const receivedVal = {
-  //   labels: ['W', 'REST'],
-  //   values: ['320\n20\n10', '20\n20\n20'],
-  // };
-
   const returnedActual: any = [];
   for (let i = 0; i < receivedVal.values.length; i++) {
     const values = receivedVal.values[i].split(/\r?\n/);
@@ -16,9 +11,6 @@ const convertToActualData = (receivedVal: any): any => {
       }
     }
   }
-
-  // const returnedActual = ['W320/REST20', 'W20/REST20', 'W10/REST20'];
-
   return returnedActual;
 };
 
